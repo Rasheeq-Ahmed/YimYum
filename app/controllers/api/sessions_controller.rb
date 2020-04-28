4041dd9002974ed7e4ce36bd824dacbfe667c15d
Rasheeq <contact@rasheeqahmed.com>
2020-04-28 11:46:27 -0700

    backend auth

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,22 @@
 class Api::SessionsController < ApplicationController
+
+    def create
+        @user = User.find_by_credentials(
+            params[:user][:username],
+            params[:user][:password]
+        )
+        if @user
+            login!(@user)
+            render 'api/users/show'
+        else
+            render json: ['Sorry, can not log you in!'], status: 422
+        end
+    end
+
+
+    def destroy
+        logout!
+        render json: ['Logout successful']
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,41 @@
 class User < ApplicationRecord
+
+    validates :username, :password_digest, :session_token, presence: true
+    validates :username, :email, uniqueness: true
+    validates :password, length: {minimum: 6}, allow_nil: true
+
+    after_initialize :ensure_session_token
+
+
+    spire
+
+    def self.find_by_credentials(username, pw)
+        @user = User.find_by(username: username)
+        return nil if user.nil?
+        @user.is_password?(pw) ? user : nil
+    end
+
+    def password=(pw)
+     @password = pw
+     self.password_digest = BCrypt::Password.create(pw)   
+    end
+
+    def is_password?(pw)
+        BCrypt::Password.new(self.password_digest).is_password?(pw)
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom.urlsafe_base64
+        self.save!
+        self.session_token
+    end
+
+    private
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom.urlsafe_base64
+    end
+
+
+
 end
