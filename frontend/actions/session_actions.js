a2920db0a67c3ea9e21f739e4a61d38068d507fc
Rasheeq <contact@rasheeqahmed.com>
2020-04-28 15:19:43 -0700

    create session util,action, and reducer. create user reducer

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,33 @@
+import {login, signup, logout} from '../util/session_api_util';
+
+
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
+
+const receiveCurrentUser = user => ({
+    type: RECEIVE_CURRENT_USER,
+    user
+});
+
+const logoutCurrentUser = () => ({
+    type: LOGOUT_CURRENT_USER,
+});
+
+ const receiveErrors = errors => ({
+    type: RECEIVE_SESSION_ERRORS,
+    errors
+})
+
+
+export const createNewUser = formUser => dispatch => signup(formUser)
+    .then(user => dispatch(receiveCurrentUser(user)));
+
+export const login = formUser => dispatch => login(formUser)
+    .then(user => dispatch(receiveCurrentUser(user)));
+
+export const logout = () => dispatch => logout()
+    .then(() => dispatch(logoutCurrentUser()));
+
+    
