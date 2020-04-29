1a30ab02a253337fa8a56da9b74058ba97d17550
Rasheeq <contact@rasheeqahmed.com>
2020-04-29 11:37:50 -0700

    update entry point

--- frontend/yim_yum.jsx
+++ frontend/yim_yum.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import configureStore from '../frontend/store/store'
+import configureStore from '../frontend/store/store';
+import Root from './components/root';
 
 
 
@@ -9,5 +10,5 @@ document.addEventListener('DOMContentLoaded', () => {
     const store = configureStore();
     window.getState = store.getState;
     window.dispatch = store.dispatch;
-    ReactDOM.render(<h1>Welcome to YimYum</h1>, root)
+    ReactDOM.render(<Root store={store}/>, root)
 })
