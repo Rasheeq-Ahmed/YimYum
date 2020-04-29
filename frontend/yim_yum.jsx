4f93984069835d97cc72b8003f15ad4a384150a9
Rasheeq <contact@rasheeqahmed.com>
2020-04-29 10:16:11 -0700

    fix npm install issues add root reducer and store

--- frontend/yim_yum.jsx
+++ frontend/yim_yum.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-
+import configureStore from '../frontend/store/store'
 
 
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
-    // const store = configureStore();
+    const store = configureStore();
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
     ReactDOM.render(<h1>Welcome to YimYum</h1>, root)
 })
