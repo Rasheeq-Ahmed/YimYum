56994b9b26ec66dd0bc0c49d9da2ef3642e68bb6
Rasheeq <contact@rasheeqahmed.com>
2020-07-02 00:07:48 -0700

    fixing styling on splash

--- frontend/components/nav_bar/footer_nav.jsx
+++ frontend/components/nav_bar/footer_nav.jsx
@@ -7,16 +7,22 @@ import {SocialIcon} from 'react-social-icons';
 class FooterNav extends React.Component {
     render() {
         return (
-            <div className="prof-links">
-                <div>
-                <SocialIcon url="https://www.linkedin.com/in/rasheeq-ahmed-53b7aa91/" bgColor="#ffffff"/>
-                </div>
-                <div>
-
-                <SocialIcon url="https://github.com/Rasheeq-Ahmed/" bgColor="#ffffff"/>
-                </div>
+          <div className="prof-links">
+            <div>
+              <SocialIcon
+                url="https://www.linkedin.com/in/rasheeq-ahmed-53b7aa91/"
+                bgColor="#ffffff"
+              />
             </div>
-        )
+            <div>
+              <SocialIcon
+                url="https://github.com/Rasheeq-Ahmed/"
+                bgColor="#ffffff"
+              />
+            </div>
+            <div className="trademark">© 2020 YimYum</div>
+          </div>
+        );
     }
 }
 
--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -13,44 +13,58 @@ class Splash extends React.Component {
     render() {
 
         return (
-
-            <div className="splash-all">
-                <div className="splash-header">
-                    {/* <Link to="/trending">
+          <div className="splash-all">
+            <div className="splash-header">
+              {/* <Link to="/trending">
                     <div className="watch-btn">Watch Now</div>
                     </Link> */}
-                    <NavBarContainer/>
-                </div>
-                <div className='splash-body'>
-                    <div>
-                        <h1 className='splash-slogan'>Satisfy Your Cravings</h1>
-                    </div>
-                    <div>
-                        <p className="splash-subslogan">Real Noms. Real Yums.</p>
-                    </div>
-                        {/* <div className="splash-videos">
-                            <video className="sushi" autoPlay muted loop>
-                            <source src="https://i.imgur.com/CeLID8W.mp4" type="video/mp4"/>
-                            </video>
-                            <video className="cheeseyums" autoPlay muted loop>
-                            <source src="https://i.imgur.com/Apgt1hz.mp4" type="video/mp4"/>
-                            </video>
-                            <video className="colordrip" autoPlay muted loop>
-                            <source src="https://i.imgur.com/2N0UPWP.mp4" type="video/mp4"/>
-                            </video>
-                            <video className="chicken" autoPlay muted loop>
-                            <source src="https://i.imgur.com/a8fhCKJ.mp4" type="video/mp4"/>
-                            </video>
-                            <video className="dessert" autoPlay muted loop>
-                            <source src="https://i.imgur.com/Q6P8Yf4.mp4" type="video/mp4"/>
-                            </video>
-                        </div> */}
-                </div>
-                <div className='splash-footer'>
-                    <FooterNav/>
-                </div>
+              <NavBarContainer />
             </div>
-        )
+              <div className="splash-title">
+                <h1 className="splash-slogan">Satisfy Your Cravings</h1>
+                <p className="splash-subslogan">Real Noms. Real Yums.</p>
+              </div>
+            <div className="splash-body">
+              <div className="splash-videos">
+                {/* <div className="splash-vids"> */}
+                  <video className="splash-vid" autoPlay muted loop>
+                    <source
+                      src="https://i.imgur.com/CeLID8W.mp4"
+                      type="video/mp4"
+                    />
+                  </video>
+                  <video className="splash-vid" autoPlay muted loop>
+                    <source
+                      src="https://i.imgur.com/Apgt1hz.mp4"
+                      type="video/mp4"
+                    />
+                  </video>
+                  <video className="splash-vid" autoPlay muted loop>
+                    <source
+                      src="https://i.imgur.com/2N0UPWP.mp4"
+                      type="video/mp4"
+                    />
+                  </video>
+                  <video className="splash-vid" autoPlay muted loop>
+                    <source
+                      src="https://i.imgur.com/a8fhCKJ.mp4"
+                      type="video/mp4"
+                    />
+                  </video>
+                  <video className="splash-vid" autoPlay muted loop>
+                    <source
+                      src="https://i.imgur.com/Q6P8Yf4.mp4"
+                      type="video/mp4"
+                    />
+                  </video>
+                
+              </div>
+            </div>
+            <div className="splash-footer">
+              <FooterNav />
+            </div>
+          </div>
+        );
    
     }
 
