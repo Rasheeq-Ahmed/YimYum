dc1e5ecdfe555f71decb57e1c43d7171146c0fc0
Rasheeq <contact@rasheeqahmed.com>
2020-07-02 21:31:15 -0700

    add sidebar

--- frontend/components/videos/video_index/video_index.jsx
+++ frontend/components/videos/video_index/video_index.jsx
@@ -18,43 +18,59 @@ class VideoIndex extends React.Component {
         
         // console.log(this.props)
         return (
-            <div className='vIndex-all'>
-                <div className='vIndex-header'>
-                <NavBarContainer/>
-                </div>
-                <div className='vIndex-body'> 
-                    <div className='vIndex-left'>
-                        <div className="vIndex-nav">
-                            <h1 className="foryou">For You</h1>
-                        </div>
-                        <div className="vIndex-follow">
-
-                        </div>
-
-                        <div className="vIndex-prof">
-
-                        </div>
-
-                    {/* <h1>Trending</h1>
-                    <p>Watch the latest videos from our community</p> */}
+          <div className="vIndex-all">
+            <div className="vIndex-header">
+              <NavBarContainer />
+            </div>
+            <div className="vIndex-body">
+              <div className="vIndex-left">
+                <div className="vIndex-nav">
+                    <div className="nav-foryou">
+                        <img src={window.house} alt="for-you" />
+                        <h1 className="foryou">For You</h1>
                     </div>
+                    <div className="nav-follow">
+                        <img src={window.following} alt="follow" />
+                        <h1 className="following">Following</h1>
+                    </div>
+                </div>
+                <div className="vIndex-follow">
+                    <h1 className="top-accounts">Your top accounts</h1>
+                    <div className='follow-accts'>
+                        <div className="follow-pic">
+                            
+                        </div>
+                        <div className="follow-info">
+                            <p>Username</p>
+                            <p>Name</p>
+                        </div>
+                    </div>
+                    <div className="see-all">
+                        <p>See All</p>
+                        <img src={window.dropdownArrow}/>
+                    </div>
+                </div>
 
-                    <div className="vIndex-mid">
-                        <div className='video-body'> 
-                            {/* {this.props.videos.map((video, idx) => (
+                <div className="vIndex-prof"></div>
+
+                {/* <h1>Trending</h1>
+                    <p>Watch the latest videos from our community</p> */}
+              </div>
+
+              <div className="vIndex-mid">
+                <div className="video-body">
+                  {/* {this.props.videos.map((video, idx) => (
                                 <VideoIndexItem  key={idx} video={video}/>
                             ))} */}
-                        </div>
-                    </div>
-                   
                 </div>
-                
-                <div className='vIndex-footer'>
-                    <FooterNav/>
-                </div>
-
+              </div>
             </div>
-        )
+
+            <div className="vIndex-footer">
+              <FooterNav />
+            </div>
+          </div>
+        );
     }
 }
 
