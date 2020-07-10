5947bd9b4d8b21fc50ec8b996f9cff32ce94d75b
Rasheeq <contact@rasheeqahmed.com>
2020-07-10 00:01:11 -0700

    add comments table

---
+++ db/migrate/20200710065333_create_comments.rb
@@ -0,0 +1,11 @@
+class CreateComments < ActiveRecord::Migration[5.2]
+  def change
+    create_table :comments do |t|
+      t.integer :user_id, null: false
+      t.integer :video_id, null: false
+      t.text :body, null: false
+    end
+    add_index :comments, :user_id
+    add_index :comments, :video_id
+  end
+end
