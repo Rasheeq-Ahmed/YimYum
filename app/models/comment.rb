9a350be17f7a0e078f0c18063448d53bcd27134c
Rasheeq <contact@rasheeqahmed.com>
2020-07-10 00:05:33 -0700

    add comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,21 @@
 class Comment < ApplicationRecord
+
+    validates :user_id, :video_id, :body, presence: true
+
+
+    belongs_to :user,
+    primary_key: :id,
+    foreign_key: :user_id,
+    class_name: "User"
+
+    belongs_to :video,
+    primary_key: :id,
+    foreign_key: :video_id,
+    class_name: "Video"
+
+    has_many :likes,
+        foreign_key: :comment_id,
+        class_name: "Like" 
+    
+
 end
