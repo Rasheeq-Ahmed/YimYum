166faff15eb704af3e5d8fa516cee5e84493020b
Rasheeq <contact@rasheeqahmed.com>
2020-07-14 03:21:03 -0700

    follow util

---
+++ frontend/util/follow_api_util.js
@@ -0,0 +1,33 @@
+export const fetchAllFollows = () => {
+    return (
+        $.ajax({
+            url: `/api/follows`,
+            method: "GET"
+        })
+    )
+}
+export const fetchFollow = (id) => {
+    return (
+        $.ajax({
+            url: `/api/follows/${id}`,
+            method: "GET"
+        })
+    )
+}
+export const createFollow = (follow) => {
+    return (
+        $.ajax({
+            url: `/api/follows/`,
+            method: "POST",
+            data: {follow}
+        })
+    )
+}
+export const deleteFollows = (followId) => {
+    return (
+        $.ajax({
+            url: `/api/follows/${followId}`,
+            method: "DELETE"
+        })
+    )
+}
